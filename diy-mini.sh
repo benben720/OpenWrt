8604b1ca2e52c0e9f61bc982e00e07da5d311a21
benben720 <67833264+benben720@users.noreply.github.com>
2024-07-27 18:05:09 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -40,6 +40,8 @@ git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/lua-maxminddb
+git clone --depth=1 https://github.com/VictC79/luci-app-vssr package/luci-app-vssr
 
 # Themes
 # git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
