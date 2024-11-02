a68adb145a57f47433dd453139542ea2bc4b2b02
benben720 <67833264+benben720@users.noreply.github.com>
2024-11-02 18:00:32 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -36,8 +36,8 @@ git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/luci-app
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 # git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/lua-maxminddb
